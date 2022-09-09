dcb48f7e8e87972a374736985f19a5768da3bfc5
ChandanCD <chandanncharan@gmail.com>
2022-09-09 13:19:53 +0530

    feat: add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'list', component: AppComponent }
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
